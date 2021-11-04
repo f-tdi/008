5b65abd8b4fbd7448dc8d8a2bb5e951c6f1deddd
f-tdi <87537152+f-tdi@users.noreply.github.com>
2021-11-04 16:22:08 +0000

    Update gestures.js

--- project_1/scripts/gestures.js
+++ project_1/scripts/gestures.js
@@ -4,8 +4,8 @@ AFRAME.registerComponent("gesture-handler", {
   schema: {
     enabled: { default: true },
     rotationFactor: { default: 2 },
-    minScale: { default: 1 },
-    maxScale: { default: 2 },
+    minScale: { default: 0.3 },
+    maxScale: { default: 1.5 },
   },
 
   init: function () {
