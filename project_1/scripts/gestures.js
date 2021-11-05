5236c1a224857c3958e25864c39f19113bc2fa2c
f-tdi <87537152+f-tdi@users.noreply.github.com>
2021-11-05 10:00:17 +0000

    Update gestures.js

--- project_1/scripts/gestures.js
+++ project_1/scripts/gestures.js
@@ -40,9 +40,10 @@ AFRAME.registerComponent("gesture-handler", {
     this.el.sceneEl.removeEventListener("twofingermove", this.handleScale);
   },
 
-  
   handleRotation: function (event) {
     if (this.isVisible) {
+      this.el.object3D.rotation.y +=
+        event.detail.positionChange.x * this.data.rotationFactor;
       this.el.object3D.rotation.x +=
         event.detail.positionChange.y * this.data.rotationFactor;
     }
@@ -231,4 +232,3 @@ AFRAME.registerComponent("gesture-detector", {
     return numberNames[Math.min(touchCount, 4) - 1];
   }
 });
-
