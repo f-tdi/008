6e4ee393f03361c74e2f3c4108b3ae024b34c458
f-tdi <87537152+f-tdi@users.noreply.github.com>
2021-11-10 17:09:00 +0000

    Update events.js

--- project_1/scripts/events.js
+++ project_1/scripts/events.js
@@ -1,11 +1,15 @@
 AFRAME.registerComponent('markerhandler', {
-
+    
     init: function() {
         const animatedMarker = document.querySelector("#animated-marker");
- 
+        const aEntity = document.querySelector("#animated-model");
+
+        // every click, we make our model grow in size :)
+
         animatedMarker.addEventListener('click', function(ev, target){
+            const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+            if (aEntity && intersectedElement === aEntity) {
                 alert('Clicked!')
             }
         });
-}});
 
