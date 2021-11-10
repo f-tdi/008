8be8ac1bb5fbec788d572b28e994ee882152b2a4
f-tdi <87537152+f-tdi@users.noreply.github.com>
2021-11-10 17:42:18 +0000

    Create events_2.js

---
+++ project_1/scripts/events_2.js
@@ -0,0 +1,9 @@
+AFRAME.registerComponent('markerhandler', {
+    
+    init: function() {
+        init: function() {
+                this.el.addEventListener('click', () => {
+                    alert('Clicked!')
+ 		    //window.location.assign("https://www.w3schools.com")
+                });
+        }});
